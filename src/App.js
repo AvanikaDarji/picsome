9c7de31f799188128b3324f188dd41de8504ad31
Avanika <avanikadarji@gmail.com>
2021-03-04 22:57:22 -0800

    add redirect to photos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import { Link, Route, Switch } from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import { Link, Route, Switch, Redirect } from "react-router-dom";
 import Photos from "./pages/Photos";
 import Cart from "./pages/Cart";
 
-
-
 function App() {
-    return ( <div className = "App" >
-    
-<Header/>
-<Switch>
-<Route path="/photos"><Photos/></Route>
-<Route path="/cart"><Cart/></Route>
-</Switch>
-
-         </div>
-        
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Redirect exact from="/" to="/photos" />
+        <Route exact path="/photos">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          {" "}
+          <Cart />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
